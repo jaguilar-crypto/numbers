17ee999a10e1c1712d47a5aa7ab2a5fb2638f426
Joana Aguilar <jaguilar@chapman.edu>
2025-10-27 13:19:50 -0700

    Add files via upload

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# numbers.sh - Print numbers from 1 to N with Odd/Even labels
+
+read -p "Please enter a positive integer: " number
+
+
+if ! [[ "$number" =~ ^[1-9][0-9]*$ ]]; then
+    echo "Error: Please enter a valid positive integer."
+    exit 1
+fi
+
+for (( i=1; i<=number; i++ ))
+do
+    if (( i % 2 == 0 )); then
+        echo "$i Even"
+    else
+        echo "$i Odd"
+    fi
+done
